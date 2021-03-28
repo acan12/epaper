cd22c8df0f02a03f349c7dea256fc420aa40d899
taufiq <taufiq@clappingape.com>
2021-03-28 23:40:51 +0700

    update layout for epaper main

--- app/src/main/java/app/epaper/com/bolang/ui/activity/MainActivity.kt
+++ app/src/main/java/app/epaper/com/bolang/ui/activity/MainActivity.kt
@@ -1,13 +1,86 @@
 package app.epaper.com.bolang.ui.activity
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import app.beelabs.com.codebase.base.BaseActivity
-import app.epaper.com.bolang.R
+import app.beelabs.com.codebase.support.rx.RxTimer
+import app.epaper.com.bolang.ui.tools.UiUtil
+import app.epaper.com.bolang.databinding.ActivityMainBinding
+import com.github.barteksc.pdfviewer.listener.OnFileDownloadCompleteListener
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener
+import java.io.File
 
-class MainActivity : BaseActivity() {
+
+class MainActivity : BaseActivity(), OnPageChangeListener, OnLoadCompleteListener,
+    OnFileDownloadCompleteListener {
+    private lateinit var binding: ActivityMainBinding
+    private val PERMISSION_CODE: Int = 42042
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val permissionCheck = ContextCompat.checkSelfPermission(
+            this,
+            READ_EXTERNAL_STORAGE
+        )
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(READ_EXTERNAL_STORAGE),
+                PERMISSION_CODE
+            )
+            return
+        }
+
+        RxTimer.doTimer(5000, false, object: RxTimer(){
+            override fun onCallback(along: Long?) {
+                binding.progressBar.visibility = View.GONE
+                setupUI()
+            }
+        })
+
+    }
+
+    private fun setupUI() = with(binding) {
+//        pdfView.fromAsset("pdf/epaper_demo.pdf")
+//            .pages(0, 2, 1, 3, 3, 3) // all pages are displayed by default
+        pdfView.fromUrl("http://www.africau.edu/images/default/sample.pdf")
+            .defaultPage(0)
+            .enableSwipe(true) // allows to block changing pages using swipe
+            .enableDoubletap(true)
+            .onLoad(this@MainActivity) // called after document is loaded and starts to be rendered
+            .onPageChange(this@MainActivity)
+            .swipeHorizontal(false)
+            .enableAntialiasing(false)
+            .spacing(10)
+            .onFileDownload(this@MainActivity)
+            .load()
+
+        RxTimer.doTimer(5000, false, object: RxTimer(){
+            override fun onCallback(along: Long?) {
+                pdfView.jumpTo(3, true)
+            }
+        })
+
+    }
+
+    override fun onPageChanged(page: Int, pageCount: Int) {
+        Toast.makeText(this@MainActivity, "Page = $page", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun loadComplete(nbPages: Int) {
+
+    }
+
+    override fun onDownloadComplete(file: File?) {
+
     }
 }
---
+++ app/src/main/java/app/epaper/com/bolang/ui/tools/UiUtil.kt
@@ -0,0 +1,11 @@
+package app.epaper.com.bolang.ui.tools
+
+import android.app.Activity
+import android.view.View
+
+object UiUtil {
+
+    fun setupLightStatusBarMode(activity: Activity) {
+        activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+    }
+}
