ee98506b99ff6a1411edd9a12a94b2249b6afa20
ary <ary@clappingape.com>
2021-06-01 12:05:17 +0700

    update fix search list pdf edition

--- app/src/main/java/app/epaper/com/bolang/ui/fragment/ListPdfEditionFragment.kt
+++ app/src/main/java/app/epaper/com/bolang/ui/fragment/ListPdfEditionFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.widget.doAfterTextChanged
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import app.beelabs.com.codebase.base.BaseFragment
 import app.beelabs.com.codebase.base.response.BaseResponse
 import app.epaper.com.bolang.App
+import app.epaper.com.bolang.IConfig
 import app.epaper.com.bolang.R
 import app.epaper.com.bolang.databinding.FragmentListPdfeditionBinding
 import app.epaper.com.bolang.model.entity.Content
@@ -20,13 +23,13 @@ import app.epaper.com.bolang.ui.dialog.SubscribeOfferDialog
 import app.epaper.com.bolang.ui.impl.IHomeView
 
 class ListPdfEditionFragment : BaseFragment(), IHomeView {
-    private lateinit var binding : FragmentListPdfeditionBinding
+    private lateinit var binding: FragmentListPdfeditionBinding
     private var loading = false
     private var nextPage = 1
     private var totalData = 0
 
-    private lateinit var query: String
-    private lateinit var sortby: String
+    private var query: String = ""
+    private var sortby: String = "desc"
 
 
     override fun onCreateView(
@@ -42,12 +45,41 @@ class ListPdfEditionFragment : BaseFragment(), IHomeView {
         setupUI()
     }
 
-    private fun setupUI() = with(binding){
+    private fun setupUI() = with(binding) {
         btnBack.setOnClickListener {
             App.getNavigationComponent().homeNavigation().navigateBackListEdition(it)
         }
-        btnSort.setOnClickListener {
+        inputSearchBox.doAfterTextChanged {
+            nextPage = 1
+            query = it.toString()
             doListReleaseEdition(query, sortby, nextPage)
+        }
+        btnSort.setOnClickListener {
+            it.tag.apply {
+                nextPage = 1
+                sortby = this.toString()
+                doListReleaseEdition(query, sortby, nextPage)
+
+                if (sortby == IConfig.SORT_ASC) {
+                    it.tag = IConfig.SORT_DESC
+                    (it as TextView).setCompoundDrawablesWithIntrinsicBounds(
+                        R.drawable.ic_sort_asc,
+                        0,
+                        0,
+                        0
+                    );
+                    it.text = resources.getString(R.string.oldest)
+                } else {
+                    it.tag = IConfig.SORT_ASC
+                    (it as TextView).setCompoundDrawablesWithIntrinsicBounds(
+                        R.drawable.ic_sort_desc,
+                        0,
+                        0,
+                        0
+                    );
+                    it.text = resources.getString(R.string.latest)
+                }
+            }
         }
         var layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
         rvList.layoutManager = layoutManager
@@ -72,6 +104,7 @@ class ListPdfEditionFragment : BaseFragment(), IHomeView {
     }
 
     private fun doListReleaseEdition(q: String, sortby: String, page: Int) {
+        binding.itemProgressbar.visibility = View.VISIBLE
         ResourcePresenter(this@ListPdfEditionFragment).onListContent(q, sortby, page.toString())
     }
 
@@ -79,12 +112,14 @@ class ListPdfEditionFragment : BaseFragment(), IHomeView {
         var result = data as ContentResponse
         with(binding) {
             if (result.contents.isNotEmpty()) {
+                rvList.visibility = View.VISIBLE
                 itemProgressbar.visibility = View.GONE
                 itemProgressbarMessage.visibility = View.GONE
                 rvList.adapter = EpaperEditionAdapter(result.contents, this@ListPdfEditionFragment)
 
                 nextPage++
             } else {
+                rvList.visibility = View.GONE
                 itemProgressbar.visibility = View.INVISIBLE
                 itemProgressbarMessage.text = resources.getString(R.string.data_not_found)
             }
